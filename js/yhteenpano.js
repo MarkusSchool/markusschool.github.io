93d89c6349c6cf7a8a1e2b8dc737e92b765accb9
MarkusSchool <nokelainennmarkus@gmail.com>
2023-03-22 12:48:29 +0200

    lisätty että ostoskoris näkyy yhteishinta

--- js/yhteenpano.js
+++ js/yhteenpano.js
@@ -6,11 +6,19 @@ const hintayhtHtml = document.getElementById("hintayht")
 document.addEventListener("DOMContentLoaded", function() {
 
     const pizzaArray = JSON.parse(localStorage.getItem("pizzaArray"));
-
-    console.log(pizzaArray);
     
     if (pizzaArray && pizzaArray.length > 0) {
-      console.log(pizzaArray)
+      console.log("arrayssa on jotain")
+
+      pizzaArray.forEach((dict) => { // päästään dictionaryihin käsiksi, jotka ovat pizza arrayn sisällä
+        console.log(dict); 
+        hintayht += dict.hinta //Lisätään yhteiseen hintaan dictionaryssa oleva hinta
+      });
+
+      console.log(hintayht) //Printataan yhteishinta
+
+      hintayhtHtml.textContent = `Yhteishinta: ${hintayht}€`
+
     }
     
 });
