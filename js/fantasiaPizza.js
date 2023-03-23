037120dfa8fe83f44722070304b2056870665618
onnikeuda <onni.raappana@edu.keuda.fi>
2023-03-23 19:13:55 +0200

    muutoskai fantasiaan

--- js/fantasiaPizza.js
+++ js/fantasiaPizza.js
@@ -1,9 +1,13 @@
-const numberOfPizzas = 2; // Replace this with the actual number of pizzas chosen
+// retrieve the value from localStorage
+var numOfPizzas = Number(localStorage.getItem("fantasia-maara"));
 
-const maara = document.querySelector('.maara');
-const h3 = document.createElement('h3');
+// select the HTML element and set its innerHTML to the value of numOfPizzas
+document.getElementById("numOfPizzas").innerHTML = numOfPizzas;
 
-if (numberOfPizzas > 1) {
-  h3.textContent = `#${numberOfPizzas}`;
-  maara.appendChild(h3);
+// select the container element
+var container = document.getElementById("numOfPizzasContainer");
+
+// hide the container element if numOfPizzas is less than or equal to 1
+if (numOfPizzas <= 1) {
+  container.style.display = "none";
 }
